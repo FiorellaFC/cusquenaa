167e4d3f17b893dad90d73cb163d07b7ea7bb367
FiorellaFC <nicollfajardo88@gmail.com>
2025-11-28 13:53:57 -0500

    Actualización de datos

--- frontend/pages/sidebear_admin.php
+++ frontend/pages/sidebear_admin.php
@@ -2,39 +2,50 @@
     <div class="sb-sidenav-menu">
         <div class="nav">
             <div class="sb-sidenav-menu-heading">Gestión</div>
+
+            <!-- Usuarios -->
             <a class="nav-link" href="usuarios.php">
-                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                <div class="sb-nav-link-icon"><i class="fas fa-users-cog"></i></div>
                 Usuarios
             </a>
-            <a class="nav-link" href="gestioncitas.php">
-                <div class="sb-nav-link-icon"><i class="fas fa-balance-scale"></i></div>
-                Reservas
+
+            <!-- Citas -->
+            <a class="nav-link" href="gestion_citas.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-calendar-check"></i></div>
+                Citas
             </a>
-           
-            <a class="nav-link" href="gestion_citas.php   ">
-                <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
-                citas
+
+            <!-- Clientes -->
+            <a class="nav-link" href="gestion_clientes.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-user-friends"></i></div>
+                Clientes
             </a>
-             <a class="nav-link" href="gestion_clientes.php   ">
-                <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
-                clientes
+
+            <!-- Contacto -->
+            <a class="nav-link" href="gestionContacto.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-envelope-open-text"></i></div>
+                Contacto
             </a>
-             <a class="nav-link" href="vistaContacto.php   ">
-                <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
-                contacto
+
+            <!-- Promociones -->
+            <a class="nav-link" href="promociones_interno.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-tags"></i></div>
+                Promociones
             </a>
-            <a class="nav-link" href="promociones_interno.php   ">
-                <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
-                promociones
+
+            <!-- Reportes -->
+            <a class="nav-link" href="reportes.php">
+                <div class="sb-nav-link-icon"><i class="fas fa-file-chart-line"></i></div>
+                Reportes
             </a>
-            
-        </div>
-    </div>
-    <div class="sb-sidenav-footer">
-        <div class="small">Sesion Inciada como:</div>
-        administrador
-    </div>
-</nav>
+
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Sesion Inciada como:</div>
+                    Administrador
+                </div>
+            </nav>
 
 <script>
     // Control del toggle
